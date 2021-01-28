53f4e20e6605e3245df72d14359a1d1bbbc1bce6
Zhane Zabala <zhanezabala97@gmail.com>
2021-01-27 18:49:52 -0800

    schema sql is written

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,12 @@
-CREATE DATABASE burgers_db
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id AUTO_INCREMENT INT PRIMARY KEY,
+    burger_name VARCHAR(100) NOT NULL,
+    devoured BOOLEAN,
+
+)
